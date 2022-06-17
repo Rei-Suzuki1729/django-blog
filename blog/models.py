4cde62bf07691f4350cd5dd814b2c3be642c9bd3
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-06-17 16:13:57 +0900

    feat_upload_picture & feat_下書き機能

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from turtle import title, update
 from django.db import models
 
@@ -8,6 +9,7 @@ class Post(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   update_at = models.DateTimeField(auto_now=True)
   is_published = models.BooleanField(default=False)
+  image = models.ImageField(upload_to="uploads/",null=True,blank=True )
 
 def _str_(self):
   return self.title
